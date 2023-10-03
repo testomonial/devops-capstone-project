b6d33e30d2614f1bd062587e09aa81045dc61e70
testomonial <mastera11780@gmail.com>
2023-10-03 15:58:44 -0400

    added read account route and testcases

--- service/routes.py
+++ service/routes.py
@@ -70,6 +70,21 @@ def create_accounts():
 
 # ... place you code here to READ an account ...
 
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """
+    Reads an Account by id
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    print("account id in route: ", account_id)
+    account = Account.find(account_id)
+
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"No Account Exists with this id")
+    
+    return account.serialize(), status.HTTP_200_OK
+
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
