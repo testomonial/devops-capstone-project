456162bc40afa18b9372d9e0c27cb09beba7b47b
testomonial <mastera11780@gmail.com>
2023-10-04 15:49:02 -0400

    added talisman lib

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,17 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
+
+@app.after_request
+def add_xss_protection_header(response):
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    return response
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
